5baa8f67187182988cdb1f21900e9d8d854083e0
Tran Van Minh Tri <tvmthtr@gmail.com>
2025-03-28 18:42:38 +0700

    Update staff

--- src/main/java/com/store/app/petstore/entities/Staff.java
+++ src/main/java/com/store/app/petstore/entities/Staff.java
@@ -10,10 +10,11 @@ public class Staff {
     private String email;
     private double salary;
     private LocalDate hireDate;
-    private String role; // "sales", "warehouse", "care"
+    private String role;
     private boolean isActive;
 
-    public Staff(int staffId, int userId, String fullName, String phone, String email, double salary, LocalDate hireDate, String role, boolean isActive) {
+    public Staff(int staffId, int userId, String fullName, String phone, String email, double salary,
+            LocalDate hireDate, String role, boolean isActive) {
         this.staffId = staffId;
         this.userId = userId;
         this.fullName = fullName;
@@ -25,5 +26,75 @@ public class Staff {
         this.isActive = isActive;
     }
 
-    // Getters và Setters
+    public int getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
