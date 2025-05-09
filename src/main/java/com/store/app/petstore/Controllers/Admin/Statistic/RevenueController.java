4f42a7468680498e6b8a8f5665116d5b7371c274
Your Name <tvmthtr@gmail.com>
2025-05-09 19:23:43 +0700

    feat: add revenue statistics UI with filters and labels

--- src/main/java/com/store/app/petstore/Controllers/Admin/Statistic/RevenueController.java
+++ src/main/java/com/store/app/petstore/Controllers/Admin/Statistic/RevenueController.java
@@ -1,4 +1,10 @@
 package com.store.app.petstore.Controllers.Admin.Statistic;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
 public class RevenueController {
 }
